e62bc58424e62dd2d81e2b91a0adb4a06efb4f7b
tobin <chrisposkus@hotmail.com>
2014-02-24 15:04:08 -0700

    Updated registerp2.php
    - Can now add and remove teams (still needs validation)
    - Register button works and sends data through ajax POST. Need to figure
    out how to deal with arrays coming in through ajax POST on php side.

--- pages/registerp2.php
+++ pages/registerp2.php
@@ -17,22 +17,16 @@
       
       <!-- new row -->
       <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-6">
          <div class="form-group">
             <label for="studentNumber">Student #</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="">
-          </div>
-        </div>
-        <div class="col-md-3">
-         <div class="form-group">
-            <label for="studentNumber">S.I.N. #</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-student-number" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
         <div class="col-md-6">
          <div class="form-group">
             <label for="studentNumber">Email</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-email" type="email" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -42,19 +36,19 @@
         <div class="col-md-5">
           <div class="form-group">
             <label for="studentNumber">First Name</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-first-name" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
         <div class="col-md-5">
           <div class="form-group">
             <label for="studentNumber">Last Name</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-last-name" type="email" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
         <div class="col-md-2">
           <div class="form-group">
             <label for="studentNumber">Initial(s)</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-initials" type="email" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -65,7 +59,7 @@
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Gender</label>
-            <select class="form-control">
+            <select id="reg-gender" class="form-control">
               <option></option>
               <option>Male</option>
               <option>Female</option>
@@ -76,21 +70,21 @@
         <div class="col-md-3">
           <div class="form-group">
             <label for="monthOfBirth">Date of Birth</label>
-            <input type="email" class="form-control" id="monthOfBirth" placeholder="MM/DD/YYYY">
+            <input id="reg-date-of-birth" type="input" class="form-control" id="monthOfBirth" placeholder="MM/DD/YYYY">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Height</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="ex. 5&#39;8&#34;">
+            <input id="reg-height" type="email" class="form-control" id="studentNumber" placeholder="ex. 5&#39;8&#34;">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Weight (lbs.)</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="ex. 160">
+            <input id="reg-weight" type="email" class="form-control" id="studentNumber" placeholder="ex. 160">
           </div>
         </div>
       </div>
@@ -107,21 +101,21 @@
         <div class="col-md-6">
          <div class="form-group">
             <label for="studentNumber">Street</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-street" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">City</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-city" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Province</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-province" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -131,21 +125,21 @@
         <div class="col-md-6">
           <div class="form-group">
             <label for="studentNumber">Phone</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-phone" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Postal Code</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-postal" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Country</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-current-country" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -162,21 +156,21 @@
         <div class="col-md-6">
          <div class="form-group">
             <label for="studentNumber">Street</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-street" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">City</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-city" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Province</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-province" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -186,21 +180,21 @@
         <div class="col-md-6">
           <div class="form-group">
             <label for="studentNumber">Phone</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-phone" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Postal Code</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-postal" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="form-group">
             <label for="studentNumber">Country</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-permanent-country" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -210,54 +204,7 @@
         <div class="col-md-12">
          <div class="form-group">
             <label for="studentNumber">Home Town</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="panel panel-default">
-    <div class="panel-heading">Academic Year: 2013-2014</div>
-    <div class="panel-body">
-
-      <!-- new row -->
-      <div class="row">
-        <div class="col-md-9">
-         <div class="form-group">
-            <label for="studentNumber">Program</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="">
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="studentNumber">Year of Study</label>
-            <select class="form-control">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <!-- new row -->
-      <div class="row">
-        <div class="col-md-8">
-         <div class="form-group">
-            <label for="studentNumber">High School</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="">
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="form-group">
-            <label for="studentNumber">Year of Graduation</label>
-            <input type="email" class="form-control" id="studentNumber" placeholder="YYYY">
+            <input id="reg-hometown" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
       </div>
@@ -271,7 +218,7 @@
 
       <!-- new row -->
       <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-6">
          <div class="form-group">
             <label for="studentNumber">U of L Team</label>
             <select class="form-control">
@@ -280,31 +227,41 @@
           </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-6">
           <div class="form-group">
             <label for="studentNumber">Other Team</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-team-name" type="input" class="form-control" id="studentNumber" placeholder="">
+          </div>
+        </div>
+      </div>
+
+      <!-- new row -->
+      <div class="row">
+        <div class="col-md-5">
+         <div class="form-group">
+            <label for="studentNumber">Position</label>
+            <input id="reg-team-position" type="input" class="form-control" id="studentNumber" placeholder=""YYYY-YYYY"">
           </div>
         </div>
 
         <div class="col-md-2">
          <div class="form-group">
             <label for="studentNumber">Year</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="YYYY-YYYY">
+            <input id="reg-team-year" type="input" class="form-control" id="studentNumber" placeholder="YYYY-YYYY">
           </div>
         </div>
 
         <div class="col-md-2">
           <div class="form-group">
             <label for="studentNumber">Jersey #</label>
-            <input type="input" class="form-control" id="studentNumber" placeholder="">
+            <input id="reg-team-jersey" type="input" class="form-control" id="studentNumber" placeholder="">
           </div>
         </div>
 
         <div class="col-md-2">
           <div class="form-group">
             <label for="studentNumber">Charged</label>
-            <select class="form-control">
+            <select id="reg-team-charged" class="form-control">
               <option>yes</option>
               <option>no</option>
             </select>
@@ -319,23 +276,11 @@
         </div>
       </div>
 
-      <table class="table table-condensed">
-        <thead>
-          <tr>
-            <td>Year</td>
-            <td>Team</td>
-            <td>Position</td>
-            <td>Jersey</td>
-            <td>Charged</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody id="team-table"></tbody>
-      </table>
+      <table id="team-table" class="table table-condensed"></table>
 
-      <!-- suspensions text area -->
-      <textarea class="form-control" rows="3" placeholder="Please indicate if you are presently under suspension from any sport organization or league."></textarea>
-      <br />
+      <!-- suspensions text area 
+      <textarea id="reg-suspensions" class="form-control" rows="3" placeholder="Please indicate if you are presently under suspension from any sport organization or league."></textarea>
+      -->
     </div>
   </div>
 
@@ -368,18 +313,28 @@
     $("#register-button").click(registerClick);
   }
 
+  // == team history ==
+
   function addTeamClick(event) {
     // validate input
+    var years = $( "#reg-team-year" ).val().split("-"),
+        startYear = years[0],
+        endYear = years[1],
+        teamId = 0,
+        teamName = $( "#reg-team-name" ).val(),
+        position = $( "#reg-team-position" ).val(),
+        jersey = $( "#reg-team-jersey" ).val(),
+        charged = $( "#reg-team-charged" ).val();
 
     // store in temp array
     teamHistory.push({
-      startYear : "xxxx",
-      endYear   : "xxxx",
-      teamId    : 0,
-      teamName  : "",
-      position  : "",
-      jersey    : 0,
-      charged   : false
+      startYear : startYear,
+      endYear   : endYear,
+      teamId    : teamId,
+      teamName  : teamName,
+      position  : position,
+      jersey    : jersey,
+      charged   : charged
     });
 
     // redraw the table
@@ -387,13 +342,12 @@
   }
 
   function redrawTable() {
-    var tableString = "";
+    var tableString = "<thead><tr><td>Year</td><td>Team</td><td>Position</td><td>Jersey</td><td>Charged</td><td> </td></tr></thead><tbody>";
     for (var i = 0; i < teamHistory.length; i++) {
       tableString += "<tr>";
-
       tableString += "<td>" + teamHistory[i].startYear + "-" + teamHistory[i].endYear + "</td>";
 
-      if (teamHistory[i].teamId == 0)
+      if (teamHistory[i].teamId != 0)
         tableString += "<td>put uofl team name here</td>";
       else
         tableString += "<td>" + teamHistory[i].teamName + "</td>";
@@ -401,11 +355,108 @@
       tableString += "<td>" + teamHistory[i].position + "</td>";
       tableString += "<td>" + teamHistory[i].jersey + "</td>";
       tableString += "<td>" + teamHistory[i].charged + "</td>";
-      tableString += "<td>remove</td>";
-
+      tableString += "<td><span id='team-entry-" + i + "'>remove</span></td>";
       tableString += "</tr>";
     }
-    $('#team-table').html(tableString);
+    tableString += "</tbody>";
+    $("#team-table").html(tableString);
+
+    // set delete listeners
+    for (var j = 0; j < teamHistory.length; j++) {
+      var id = "#team-entry-" + j;
+      $(id).click(removeTeamClick);
+    }
+  }
+
+  function removeTeamClick( event ) {
+    // remove delete listeners
+    for (var j = 0; j < teamHistory.length; j++) {
+      var id = "#team-entry-" + j;
+      $(id).unbind();
+    }
+
+    // remove array entry
+    var index = event.target.id.substr(11);
+    teamHistory.splice(index, 1);
+
+    // redraw
+    redrawTable();
+  }
+
+  // == register button ==
+
+  function registerClick() {
+    var studentNum = $("#reg-student-number").val(),
+        email = $("#reg-email").val(),
+        firstName = $("#reg-first-name").val(),
+        lastName = $("#reg-last-name").val(),
+        initials = $("#reg-initials").val(),
+        gender = $("#reg-gender").val(),
+        dob = $("#reg-date-of-birth").val(),
+        height = $("#reg-height").val(),
+        weight = $("#reg-weight").val(),
+        hometown = $("#reg-hometown").val(),
+        suspensions = $("#reg-suspensions").val(),
+
+        cStreet = $("#reg-current-street").val(),
+        cCity = $("#reg-current-city").val(),
+        cProvince = $("#reg-current-province").val(),
+        cPostal = $("#reg-current-postal").val(),
+        cCountry = $("#reg-current-country").val(),
+        cPhone = $("#reg-current-phone").val(),
+
+        pStreet = $("#reg-permanent-street").val(),
+        pCity = $("#reg-permanent-city").val(),
+        pProvince = $("#reg-permanent-province").val(),
+        pPostal = $("#reg-permanent-postal").val(),
+        pCountry = $("#reg-permanent-country").val(),
+        pPhone = $("#reg-permanent-phone").val();
+
+    // send registration data to the server via ajax
+    $.ajax({
+      type     : 'POST',
+      url      : 'php/registermanager.php',
+      dataType : 'json',
+      data     : { 
+        studentNum : studentNum,
+        email      : email,
+        firstName  : firstName,
+        lastName   : lastName,
+        initials   : initials,
+        gender     : gender,
+        dob        : dob,
+        height     : height,
+        weight     : weight,
+        hometown   : hometown,
+        suspension : suspensions,
+
+        cStreet    : cStreet,
+        cCity      : cCity,
+        cProvince  : cProvince,
+        cPostal    : cPostal,
+        cCountry   : cCountry,
+        cPhone     : cPhone,
+
+        pStreet    : pStreet,
+        pCity      : pCity,
+        pProvince  : pProvince,
+        pPostal    : pPostal,
+        pCountry   : pCountry,
+        pPhone     : pPhone,
+
+        teamHist   : teamHistory
+      },
+      cache    : false,
+      success  : function( result ) {
+        console.log(result);
+      },
+      error    : function(a, b, c) {
+        console.log('Error:');
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
+    });
   }
 
   init();
