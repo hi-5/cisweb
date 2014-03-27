f775fd80ace2e2725e086c8eda8eea7bd6c2b2a6
mnpaulson <mnpaulson@gmail.com>
2014-03-27 16:07:52 -0600

    Updates to report page
    Reformatted slightly and added buttons for the two custom reports.

--- pages/reports.php
+++ pages/reports.php
@@ -1,92 +1,122 @@
 <!-- new row -->
 <div class="row">
-  <div class="col-md-3">
-    <label for="team-list">Team</label>
-    <select class="form-control" id="team-list"></select>
-  </div>
-  <div class="col-md-3">
-    <label for="order-select">Year</label>
-    <select class="form-control" id="year-select"></select>
-  </div>
-  <div class="col-md-3">
-    <label for="form-select">Report</label>
-    <select class="form-control" id="report-select"></select>
-    <input class="form-control hidden" id="new-report-name" placeholder="Enter Report Name">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Team Select</h3>
+    </div>
+    <div class="panel-body">
+     <div class="col-md-3">
+      <label for="team-list">Team</label>
+      <select class="form-control" id="team-list"></select>
+    </div>
+    <div class="col-md-3">
+      <label for="order-select">Year</label>
+      <select class="form-control" id="year-select"></select>
+    </div>
   </div>
 </div>
+</div>
 <div class="row">
-<div id="report-builder" class="hidden">
-  <div class="col-md-6">
-    <label for="avail-attributes">Available Attributes</label>
-    <select multiple class="form-control" id="avail-attributes">
-    <option value="athletes.a_studentId">Student ID</option>
-    <option value="athletes.a_lastName">Last Name</option>
-    <option value="athletes.a_firstName">First Name</option>
-    <option value="athletes.a_initials">Initials</option>
-    <option value="athletes.a_gender">Gender</option>
-    <option value="athletes.a_dob">DOB</option>
-    <option value="athletes.a_height">Height</option>
-    <option value="athletes.a_weight">Weight</option>
-    <option value="athletes.a_email">Email</option>
-    <option value="athletes.a_highSchool">High School</option>
-    <option value="athletes.a_gradYear">Grad Year</option>
-    <option value="athletes.a_program">Program</option>
-    <option value="athletehistory.ah_position">Position</option>
-    <option value="athletehistory.ah_jerseyNumber">Jersey Number</option>
-    <option value="athletehistory.ah_charged">Eligibility Charged</option>
-    <option value="YOE">Year of Eligibility</option>
-    <option value="athletes.a_cStreet">Street</option>
-    <option value="athletes.a_cCity">City</option>
-    <option value="athletes.a_cProvince">Province</option>
-    <option value="athletes.a_cPostalCode">Postal Code</option>
-    <option value="athletes.a_cCountry">Country</option>
-    <option value="athletes.a_cPhone">Phone Number</option>
-    <option value="athletes.a_pStreet">Permanent Street</option>
-    <option value="athletes.a_pCity">Permanent City</option>
-    <option value="athletes.a_pProvince">Permanent Province</option>
-    <option value="athletes.a_pPostalCode">Permanent Postal Code</option>
-    <option value="athletes.a_pCountry">Permanent Country</option>
-    <option value="athletes.a_pPhone">Permanent Phone Number</option>
-    </select>
-    <br />
-    <button type="button" class="btn btn-primary" id="add-attribute-btn"><span class='glyphicon glyphicon-arrow-down'></span> Add</button>
-    <button type="button" class="btn btn-primary" id="remove-attribute-btn"><span class='glyphicon glyphicon-arrow-up'></span> Remove</button>
-  </div>
-    <br />
-    <div class="row">
-      <div class="col-md-12">
-        <div class="col-md-6">
-          <label for="selected-attributes">Selected Attributes</label>
-          <select multiple class="form-control" id="selected-attributes"></select>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Custom Report</h3>
+    </div>
+    <div class="panel-body">
+      <div class="row">
+        <div class="col-md-3">
+          <label for="form-select">Report</label>
+          <select class="form-control" id="report-select"></select>
+          <input class="form-control hidden" id="new-report-name" placeholder="Enter Report Name">
         </div>
-        <div class="col-md-2">
+      </div>
+      <div id="report-builder" class="hidden row">
+        <div class="col-md-6">
+          <br>
+          <label for="avail-attributes">Available Attributes</label>
+          <select multiple class="form-control" id="avail-attributes">
+            <option value="athletes.a_studentId">Student ID</option>
+            <option value="athletes.a_lastName">Last Name</option>
+            <option value="athletes.a_firstName">First Name</option>
+            <option value="athletes.a_initials">Initials</option>
+            <option value="athletes.a_gender">Gender</option>
+            <option value="athletes.a_dob">DOB</option>
+            <option value="athletes.a_height">Height</option>
+            <option value="athletes.a_weight">Weight</option>
+            <option value="athletes.a_email">Email</option>
+            <option value="athletes.a_highSchool">High School</option>
+            <option value="athletes.a_gradYear">Grad Year</option>
+            <option value="athletes.a_program">Program</option>
+            <option value="athletehistory.ah_position">Position</option>
+            <option value="athletehistory.ah_jerseyNumber">Jersey Number</option>
+            <option value="athletehistory.ah_charged">Eligibility Charged</option>
+            <option value="YOE">Year of Eligibility</option>
+            <option value="athletes.a_cStreet">Street</option>
+            <option value="athletes.a_cCity">City</option>
+            <option value="athletes.a_cProvince">Province</option>
+            <option value="athletes.a_cPostalCode">Postal Code</option>
+            <option value="athletes.a_cCountry">Country</option>
+            <option value="athletes.a_cPhone">Phone Number</option>
+            <option value="athletes.a_pStreet">Permanent Street</option>
+            <option value="athletes.a_pCity">Permanent City</option>
+            <option value="athletes.a_pProvince">Permanent Province</option>
+            <option value="athletes.a_pPostalCode">Permanent Postal Code</option>
+            <option value="athletes.a_pCountry">Permanent Country</option>
+            <option value="athletes.a_pPhone">Permanent Phone Number</option>
+          </select>
           <br />
-          <br />
-          <button type="button" class="btn btn-primary" id="move-up-btn"><span class='glyphicon glyphicon-arrow-up'></span></button>
-          <br />
-          <button type="button" class="btn btn-primary" id="move-down-btn"><span class='glyphicon glyphicon-arrow-down'></span></button>
+          <button type="button" class="btn btn-primary" id="add-attribute-btn"><span class='glyphicon glyphicon-arrow-down'></span> Add</button>
+          <button type="button" class="btn btn-primary" id="remove-attribute-btn"><span class='glyphicon glyphicon-arrow-up'></span> Remove</button>
+        </div>
+        <br />
+        <div class="row">
+          <div class="col-md-12">
+            <br>
+            <div class="col-md-6">
+              <label for="selected-attributes">Selected Attributes</label>
+              <select multiple class="form-control" id="selected-attributes"></select>
+            </div>
+            <div class="col-md-2">
+              <br />
+              <br />
+              <button type="button" class="btn btn-primary" id="move-up-btn"><span class='glyphicon glyphicon-arrow-up'></span></button>
+              <br />
+              <button type="button" class="btn btn-primary" id="move-down-btn"><span class='glyphicon glyphicon-arrow-down'></span></button>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="col-md-3">
+          <label for="order-select">Order By</label>
+          <select class="form-control" id="order-select"></select>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-md-8">
+          <button type="button" class="btn btn-primary" id="excel-btn"><span class='glyphicon glyphicon-save'></span> Excel</button>
+          <button type="button" class="btn btn-primary" id="web-btn"><span class='glyphicon glyphicon-globe'></span> Web</button>
+          <button type="button" class="btn btn-primary" id="preview-btn"><span class='glyphicon glyphicon-eye-open'></span> Preview</button>
+          <button type="button" class="btn btn-success" id="new-report-btn"><span class='glyphicon glyphicon-plus'></span> New</button>
+          <button type="button" class="btn btn-danger" id="del-report-btn"><span class='glyphicon glyphicon-remove'></span> Delete</button>
+          <button type="button" class="btn btn-success hidden" id="save-report-btn"><span class='glyphicon glyphicon-plus'></span> Save</button>
+          <button type="button" class="btn btn-danger hidden" id="cancel-report-btn"><span class='glyphicon glyphicon-remove'></span> Cancel</button>
         </div>
       </div>
     </div>
-    <br />
-    <div class="col-md-3">
-      <label for="order-select">Order By</label>
-      <select class="form-control" id="order-select"></select>
-    </div>
-  </div>
+  </div> 
 </div>
-<br />
 <div class="row">
-  <div class="col-md-8">
-    <button type="button" class="btn btn-primary" id="excel-btn"><span class='glyphicon glyphicon-save'></span> Excel</button>
-    <button type="button" class="btn btn-primary" id="web-btn"><span class='glyphicon glyphicon-globe'></span> Web</button>
-    <button type="button" class="btn btn-primary" id="preview-btn"><span class='glyphicon glyphicon-eye-open'></span> Preview</button>
-    <button type="button" class="btn btn-success" id="new-report-btn"><span class='glyphicon glyphicon-plus'></span> New</button>
-    <button type="button" class="btn btn-danger" id="del-report-btn"><span class='glyphicon glyphicon-remove'></span> Delete</button>
-    <button type="button" class="btn btn-success hidden" id="save-report-btn"><span class='glyphicon glyphicon-plus'></span> Save</button>
-    <button type="button" class="btn btn-danger hidden" id="cancel-report-btn"><span class='glyphicon glyphicon-remove'></span> Cancel</button>
-  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Special Reports</h3>
+    </div>
+    <div class="panel-body">
+      <button type="button" class="btn btn-primary" id="elig-report-btn"><span class='glyphicon glyphicon-save'></span> CIS Eligibility Report</button>
+      <button type="button" class="btn btn-primary" id="sid-report-btn"><span class='glyphicon glyphicon-save'></span> SID Roster Report</button>
+    </div>
 </div>
+</div>
+
 <br />
 <div class="row">
   <div class="col-md-12" id="preview-holder"></div>
@@ -94,12 +124,12 @@
 
 <!-- Hidden info used in JS goes here -->
 <form id="web-form" method="post" action="/php/generateReport.php" target class="hidden">
-<!-- Holds JSON string for POST -->
-<input type="text" name="js" id="json-holder">
-<!-- Holds format type -->
-<input type="text" name="format" id="format-holder">
-<!-- Holds mode (custom or saved) -->
-<input type="text" name="mode" id="mode-holder" value="saved">
+  <!-- Holds JSON string for POST -->
+  <input type="text" name="js" id="json-holder">
+  <!-- Holds format type -->
+  <input type="text" name="format" id="format-holder">
+  <!-- Holds mode (custom or saved) -->
+  <input type="text" name="mode" id="mode-holder" value="saved">
 </form>
 
 <script type="text/javascript">
@@ -109,6 +139,7 @@
     populateReportList();
     cislib.managerRequest("team", "getList", undefined, populateTeamList);
 
+    //Populate the year list
     for (i = new Date().getFullYear(); i > 1900; i--) {
       $('#year-select').append($('<option />').val(i).html(i + "-" + (i+1)));
     }
@@ -126,6 +157,8 @@
     $("#cancel-report-btn").click(toggleBuilder);
     $("#save-report-btn").click(saveReport);
     $("#del-report-btn").click(deleteReport);
+    $("#elig-report-btn").click(eligReport);
+    $("#sid-report-btn").click(sidReport);
     $("#report-select").change(getReportString);
   }
 
@@ -166,17 +199,17 @@
   //Populates the report selection list with all reports in the DB
   function populateReportList () {
     $.ajax({
-        type     : 'POST',
-        url      : 'php/reportsLib.php',
-        dataType : 'json',
-        data     : {action: "getList"},
-        cache    : false,
-        success  : function(result) {
-          var reportString = "";
-          for (var i = 0; i < result.length; i++) {
-            reportString += "<option value='" + result[i].r_id + "'>" + result[i].r_name + "</option>\n";
-          }
-          $("#report-select").append(reportString);
+      type     : 'POST',
+      url      : 'php/reportsLib.php',
+      dataType : 'json',
+      data     : {action: "getList"},
+      cache    : false,
+      success  : function(result) {
+        var reportString = "";
+        for (var i = 0; i < result.length; i++) {
+          reportString += "<option value='" + result[i].r_id + "'>" + result[i].r_name + "</option>\n";
+        }
+        $("#report-select").append(reportString);
           //Puts the inital report string into the holder before the user makes a selection
           getReportString();
         },
@@ -186,7 +219,7 @@
           console.log(b);
           console.log(c);
         }
-    }); 
+      }); 
   }
 
   function toggleBuilder() {
@@ -263,21 +296,21 @@
   function getReportString() {
     var reportId = $("#report-select option:selected").val();
     $.ajax({
-        type     : 'POST',
-        url      : 'php/reportsLib.php',
-        dataType : 'json',
-        data     : {action: "getString",
-                    id: reportId},
-        cache    : false,
-        success  : function(result) {
-          $("#json-holder").val(result.r_string);
-        },
-        error    : function(a, b, c) {
-          console.log('Error:');
-          console.log(a);
-          console.log(b);
-          console.log(c);
-        }
+      type     : 'POST',
+      url      : 'php/reportsLib.php',
+      dataType : 'json',
+      data     : {action: "getString",
+      id: reportId},
+      cache    : false,
+      success  : function(result) {
+        $("#json-holder").val(result.r_string);
+      },
+      error    : function(a, b, c) {
+        console.log('Error:');
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
     }); 
   }
 
@@ -310,8 +343,8 @@
       type     : 'POST',
       url      : 'php/reportsLib.php',
       data     : {action: "add",
-                  name: reportName,
-                  string: json},
+      name: reportName,
+      string: json},
       cache    : false,
       success  : function(result) {
         toggleBuilder();
@@ -330,21 +363,21 @@
   function deleteReport() {
     var reportId = $("#report-select option:selected").val();
     $.ajax({
-        type     : 'POST',
-        url      : 'php/reportsLib.php',
-        dataType : 'json',
-        data     : {action: "getString",
-                    id: reportId},
-        cache    : false,
-        success  : function(result) {
-          
-        },
-        error    : function(a, b, c) {
-          console.log('Error:');
-          console.log(a);
-          console.log(b);
-          console.log(c);
-        }
+      type     : 'POST',
+      url      : 'php/reportsLib.php',
+      dataType : 'json',
+      data     : {action: "getString",
+      id: reportId},
+      cache    : false,
+      success  : function(result) {
+
+      },
+      error    : function(a, b, c) {
+        console.log('Error:');
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
     }); 
   }
 
@@ -407,6 +440,20 @@
     }
 
     $("#web-form").submit();
+  }
+
+  //gets eligibility report
+  function eligReport() {
+    var id = $("#team-list option:selected").val();
+    var year = $('#year-select option:selected').val();
+    window.location = "/php/eligreport.php?id=" + id + "&year=" + year;
+  }
+
+  //Gets sid roster report
+  function sidReport() {
+    var id = $("#team-list option:selected").val();
+    var year = $('#year-select option:selected').val();
+    window.location = "/php/sidreport.php?id=" + id + "&year=" + year;
   }
 
   init();
